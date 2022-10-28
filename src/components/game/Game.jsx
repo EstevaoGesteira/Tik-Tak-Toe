4260c6c50facaceb68c10cededc081e7b2681f25
Estevão Gesteira <estevaogesteira2022@gmail.com>
2022-10-28 19:26:09 -0300

    Criado lógica do jogo

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,33 +1,49 @@
 import { useState } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../gameoption/GameOption'
+import Icon from '../icon/Icon'
 
 
 function Game() {
+    //    exemplo useState abaixo
     const [gameState, setGameState] = useState(Array(9).fill(0))
+    const [playerAtual, setPlayerAtual]= useState(1)
+
+    const handleClick = (pos ) => {
+        if (gameState[pos] ===0){
+            let newGameState = [...gameState]
+            newGameState[pos] = playerAtual
+            setPlayerAtual(playerAtual * -1)
+            setGameState(newGameState)
+        }
+
+    }
     return(
         <>
-        { //Usa-se chaves para usar JavaScript no html
-        gameState.map((valor,pos) =>
-         <GameOption
-            key={`game-option-pos-${pos}`}    
-            status={valor}
-        />
-         )
-        }
-        <div className={styles.game}>
-        <GameOption />
-        <GameOption/>
-        <GameOption/>
-        <GameOption/>
-        <GameOption/>
-        <GameOption/>
-        <GameOption/>
-        <GameOption/>
-        <GameOption/>
+        <div className={styles.gameContent}>
+            <div>
+                <div className={styles.game}>
+                { //Usa-se chaves para usar JavaScript no html
+                gameState.map((valor,pos) =>
+                <GameOption key={`game-option-pos-${pos}`} status={valor} onClick={() => handleClick(pos)} />
+                ) 
+                }
+        
+                </div>
+                </div>
+            <div className={styles.gameInfo}>
+                <h4>Próximo a jogar:</h4>
+                {
+                    playerAtual === 1 && <Icon  iconName='circle'/>
+                
+                }
+                        {
+                    playerAtual === -1 && <Icon  iconName='x'/>
+                
+                }
+            </div>
         </div>
         </>
     )
-}
-
+        }
 export default Game
--- src/components/gameoption/GameOption.jsx
+++ src/components/gameoption/GameOption.jsx
@@ -1,14 +1,14 @@
 import styles from './GameOption.module.css'
 import Icon from '../icon/Icon'
 
-const GameIcon = ({iconName}) => <Icon inconName={iconName} size="25px"/>
+const GameIcon = ({iconName}) => <Icon iconName={iconName} size="25px"/>
 
-function GameOption({status} ) {
+function GameOption({status, onClick} ) {
     return(
         <>
-        <div className={styles.gameOption}>
+        <div className={styles.gameOption} onClick={onClick}>
             {
-            status===1 && <GameIcon iconName='circle' />
+            status=== 1 && <GameIcon iconName='circle' />
             }
             {
             status=== -1 && <GameIcon iconName='x' />
