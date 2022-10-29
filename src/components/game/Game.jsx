65e5dfd15b9310fa922b4c729f4d945e87910754
Estevão Gesteira <estevaogesteira2022@gmail.com>
2022-10-29 09:45:20 -0300

    Erro no verifyGame

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,29 +1,49 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './Game.module.css'
 import GameOption from '../gameoption/GameOption'
 import Icon from '../icon/Icon'
 
+const gabarito = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
 
 function Game() {
-    //    exemplo useState abaixo
     const [gameState, setGameState] = useState(Array(9).fill(0))
-    const [playerAtual, setPlayerAtual]= useState(1)
+    const [playerAtual, setPlayerAtual]= useState(-1)
+    const [ganhador,setGanhador]= useState(0)
 
-    const handleClick = (pos ) => {
-        if (gameState[pos] ===0){
+    const handleClick = (pos) => {
+        if (gameState[pos] === 0 && ganhador === 0) {
             let newGameState = [...gameState]
             newGameState[pos] = playerAtual
-            setPlayerAtual(playerAtual * -1)
             setGameState(newGameState)
         }
+     }
 
-    }
+        const verifyGame = () => {
+            gabarito.forEach((line) => {
+                const values = line.map((pos) => gameState[pos])
+                const sum = values.reduce((sum, value) => sum + value)
+                if (sum === 3 || sum === -3) setGanhador(sum / 3)
+            })
+        }
+
+            useEffect(() =>{                        
+                setPlayerAtual(playerAtual * -1)
+        }, [gameState]) 
     return(
         <>
         <div className={styles.gameContent}>
             <div>
                 <div className={styles.game}>
-                { //Usa-se chaves para usar JavaScript no html
+                { 
                 gameState.map((valor,pos) =>
                 <GameOption key={`game-option-pos-${pos}`} status={valor} onClick={() => handleClick(pos)} />
                 ) 
@@ -45,5 +65,6 @@ function Game() {
         </div>
         </>
     )
-        }
+}
+
 export default Game
